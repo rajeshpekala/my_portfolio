fa8b4ae6d8d05e0b9d5b81d48f372988d3678d21
rajeshpekala <pekalarajesh001@gmail.com>
2024-07-14 04:05:08 +0530

    added socket

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,76 +10,83 @@ import nodeJs from "../assets/nodeJs.png";
 import redux from "../assets/redux.png";
 import mongoDB from "../assets/mongoDB.png";
 import java from "../assets/java.png";
+import socket from "../assets/portfolio/socket.png"
 
 const Experience = () => {
     const technologies = [
-        {
-            id:1,
-            src: html,
-            title:"HTML",
-            style:"shadow-orange-500",
-        },
-        {
-            id:2,
-            src: css,
-            title:"CSS",
-            style:"shadow-blue-500",
-        },
-        {
-            id:3,
-            src: javascript,
-            title:"JAVASCRIPT",
-            style:"shadow-yellow-500",
-        },
-        {
-            id:4,
-            src: reactImage,
-            title:"REACT",
-            style:"shadow-cyan-500",
-        },
-        {
-            id:5,
-            src: tailwind,
-            title:"TAILWIND",
-            style:"shadow-sky-500",
-        },
-        {
-            id:6,
-            src: nodeJs,
-            title:"NODEJS",
-            style:"shadow-green-500",
-        },
-        {
-            id:7,
-            src: expressJs,
-            title:"EXPRESSJS",
-            style:"shadow-yellow-500",
-        },
-        {
-            id:8,
-            src: mongoDB,
-            title:"MONGODB",
-            style:"shadow-green-500",
-        },
-        {
-            id:9,
-            src: redux,
-            title:"REDUX",
-            style:"shadow-violet-500",
-        },
-        {
-            id:10,
-            src: github,
-            title:"GITHUB",
-            style:"shadow-gray-400",
-        },
-        {
-            id:11,
-            src: java,
-            title:"JAVA",
-            style:"shadow-gray-400",
-        },
-    ]
+      {
+        id: 1,
+        src: html,
+        title: "HTML",
+        style: "shadow-orange-500",
+      },
+      {
+        id: 2,
+        src: css,
+        title: "CSS",
+        style: "shadow-blue-500",
+      },
+      {
+        id: 3,
+        src: javascript,
+        title: "JAVASCRIPT",
+        style: "shadow-yellow-500",
+      },
+      {
+        id: 4,
+        src: reactImage,
+        title: "REACT",
+        style: "shadow-cyan-500",
+      },
+      {
+        id: 5,
+        src: tailwind,
+        title: "TAILWIND",
+        style: "shadow-sky-500",
+      },
+      {
+        id: 6,
+        src: nodeJs,
+        title: "NODEJS",
+        style: "shadow-green-500",
+      },
+      {
+        id: 7,
+        src: expressJs,
+        title: "EXPRESSJS",
+        style: "shadow-yellow-500",
+      },
+      {
+        id: 8,
+        src: mongoDB,
+        title: "MONGODB",
+        style: "shadow-green-500",
+      },
+      {
+        id: 9,
+        src: redux,
+        title: "REDUX",
+        style: "shadow-violet-500",
+      },
+      {
+        id: 10,
+        src: github,
+        title: "GITHUB",
+        style: "shadow-gray-400",
+      },
+      {
+        id: 11,
+        src: java,
+        title: "JAVA",
+        style: "shadow-blue-400",
+      },
+      {
+        id: 12,
+        src: socket,
+        title: "SOCKET.IO",
+        style: "shadow-cyan-400",
+      },
+    ];
   return (
     <div name ="experience"
     className='bg-gradient-to-b from-gray-800 to-black w-full '>
